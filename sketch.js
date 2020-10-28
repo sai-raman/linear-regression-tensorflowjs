7aa85def37dd91ce07fc74a7dc9b1a6f965b09fe
Sai Raman Kilambi <ksraman07@gmail.com>
2020-10-28 14:23:42 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,66 @@
+let x_vals = []
+let y_vals = []
+let m,b;
+const learningRate = 0.05;
+const optimizer = tf.train.sgd(learningRate);
+
+function setup() {
+  createCanvas(400, 400);
+  m = tf.variable(tf.scalar(random(1)));
+  b = tf.variable(tf.scalar(random(1)));
+}
+
+
+function predict(x){
+  const tfxs = tf.tensor1d(x);
+  // y = mx+b
+  const ys = tfxs.mul(m).add(b);
+  return ys;
+}
+
+
+function loss(predictions,labels){
+  return predictions.sub(labels).square().mean();
+}
+
+function mousePressed(){
+  let x = map(mouseX,0,width,0,1);
+  let y =  map(mouseY,0,height,1,0);
+  
+  x_vals.push(x)
+  y_vals.push(y)
+}
+
+function draw() {
+  
+  tf.tidy(()=>{
+      if(x_vals.length>0){
+  const ys = tf.tensor1d(y_vals);
+  optimizer.minimize(()=>loss(predict(x_vals),ys));
+  }
+  })
+
+    background(0);
+  stroke(255);
+  strokeWeight(8);
+  for(let i=0;i<x_vals.length;i++){
+    let pixelX = map(x_vals[i],0,1,0,width);
+    let pixelY = map(y_vals[i],0,1,height,0);
+    point(pixelX,pixelY);
+  }
+  
+  const lineX = [0,1];
+  const ys = tf.tidy(()=>predict(lineX));
+  let liney = ys.dataSync();
+
+  let x1 = map(lineX[0],0,1,0,width);
+  let x2 = map(lineX[1],0,1,0,width);
+  
+  
+  let y1 = map(liney[0],0,1,0,height);
+  let y2 = map(liney[1],0,1,0,height);
+  strokeWeight(2);
+  line(x1,y1,x2,y2);
+
+
+}
